d8badc2aedd3b8ee8c91966105b4bfe6dc66e6eb
Jules Boehle <121529529+Skoodster@users.noreply.github.com>
2023-05-07 14:50:05 -0700

    textures

---
+++ src/main/lwjglb/engine/scene/Scene.java
@@ -0,0 +1,56 @@
+package main.lwjglb.engine.scene;
+
+import main.lwjglb.engine.graph.*;
+import java.util.*;
+/**
+ * Hold 3D scene values (models, lights, etc)
+ */
+public class Scene {
+    private Map<String, Model> modelMap;
+    private Projection projection;
+    private TextureCache textureCache;
+
+    public Scene(int width, int height){
+        modelMap = new HashMap<>();
+        projection = new Projection(width, height);
+        textureCache = new TextureCache();
+    }
+    
+    public void addEntity(Entity entity){
+        String modelId = entity.getModelId();
+        Model model = modelMap.get(modelId);
+        if (model == null){
+            throw new RuntimeException("Could not find model ["+modelId+"]");
+        }
+        model.getEntitiesList().add(entity);
+    }
+
+    // For 2D stuf
+    //public void addMesh(String meshId, Mesh mesh){
+    //    meshMap.put(meshId, mesh);
+    //}
+
+    public void addModel(Model model){
+        modelMap.put(model.getId(), model);
+    }
+
+    public void cleanup(){
+        modelMap.values().stream().forEach(Model::cleanup);
+    }
+
+    public Map<String, Model> getModelMap(){
+        return modelMap;
+    }
+
+    public Projection getProjection(){
+        return projection;
+    }
+
+    public TextureCache getTextureCache(){
+        return textureCache;
+    }
+
+    public void resize(int width, int height){
+        projection.updateProjMatrix(width, height);
+    }
+}
